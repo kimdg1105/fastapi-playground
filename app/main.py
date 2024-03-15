a384ca427b9040bad27d391c58a586e4acba9d28
kimdg1105 <kimss7334@naver.com>
2024-03-15 10:14:13 +0900

    refactor: router.py 구조 개선

--- app/main.py
+++ app/main.py
@@ -1,37 +1,13 @@
-from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy.orm import Session
+import uvicorn
+from fastapi import FastAPI
 
-from app.database import engine, SessionLocal
-from app.keyword import models, schemas, service
+from app.database import engine
+from app.keyword import models, router as keyword_router
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI()  # Dependency
+app.include_router(keyword_router.router, prefix="/api/v1")
 
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.post("/keywords/", response_model=schemas.Keyword)
-def create_user(keyword_request: schemas.KeywordCreate, db: Session = Depends(get_db)):
-    return service.create_keyword(db=db, keyword=keyword_request)
-
-
-@app.get("/keywords/", response_model=list[schemas.Keyword])
-def read_users(db: Session = Depends(get_db)):
-    keywords = service.get_all(db)
-    return keywords
-
-
-@app.get("/keywords/{keyword_id}", response_model=schemas.Keyword)
-def read_user(keyword_id: str, db: Session = Depends(get_db)):
-    db_keyword = service.get_keyword(db, keyword_id=keyword_id)
-    if db_keyword is None:
-        raise HTTPException(status_code=404, detail="Keyword not found")
-    return db_keyword
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
