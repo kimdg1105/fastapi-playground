4a4256eb7591059646d1ffcaee7945fbcf419152
kimdg1105 <kimss7334@naver.com>
2024-03-25 14:53:04 +0900

    feature: keyword 모듈 리팩토링

--- app/keyword/router.py
+++ app/keyword/router.py
@@ -2,25 +2,24 @@ from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from app.keyword import schemas, service
+from app.keyword import schemas, service.KeywordService as keyword_service
 
-router = APIRouter()
-
-router.tags = ["Keyword API"]
-
-
-@router.post("/keywords/", response_model=schemas.Keyword, summary="키워드 생성")
-async def create_user(keyword_request: schemas.KeywordCreate, db: Session = Depends(get_db)):
-    return service.create_keyword(db=db, keyword=keyword_request)
+keyword_router = APIRouter()
+keyword_router.tags = ["Keyword API"]
 
 
-@router.get("/keywords/", response_model=list[schemas.Keyword], summary="키워드 목록 조회")
+@keyword_router.post("/keywords/", response_model=schemas.Keyword, summary="키워드 생성")
+async def create_user(keyword_request: schemas.KeywordCreate):
+    return keyword_service.create_keyword(keyword=keyword_request)
+
+
+@keyword_router.get("/keywords/", response_model=list[schemas.Keyword], summary="키워드 목록 조회")
 async def read_users(db: Session = Depends(get_db)):
     keywords = service.get_all(db)
     return keywords
 
 
-@router.get("/keywords/{keyword_id}", response_model=schemas.Keyword, summary="키워드 상세 조회")
+@keyword_router.get("/keywords/{keyword_id}", response_model=schemas.Keyword, summary="키워드 상세 조회")
 async def read_user(keyword_id: str, db: Session = Depends(get_db)):
     db_keyword = service.get_keyword(db, keyword_id=keyword_id)
     if db_keyword is None:
